a7c3d61641781dc4f4e49e6f236e8cd5c9bb4377
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-08-29 15:48:32 +0900

    portfoliodetailsのbase

--- src/screen/PortFolioScreen/PortFolioDetails/index.tsx
+++ src/screen/PortFolioScreen/PortFolioDetails/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PortFolio } from '../../PortFolioScreen/index'
-import { PortFolioInfo } from '../SelectPortfolio/styles'
+import { PortFolioInfo, PortFolioImage } from './styles'
 
 
 interface Props {
@@ -11,13 +11,20 @@ export const PortFolioDeatils = (props: Props) => {
 
   const { portFolio } = props
   return portFolio.id !== 0 ? (
-    <PortFolioInfo id={portFolio.id} >
-      <dl style={{textAlign: 'center'}}>ポートフォリオ詳細</dl>
-      <dl>名前: {portFolio.name}</dl>
-      <dl>内容: {portFolio.kind}</dl>
-      <dl>作成時期: {portFolio.date}</dl>
-      <dl>URL: {portFolio.url}</dl>
-    </PortFolioInfo>
+    <React.Fragment>
+      {portFolio.img ? (
+        <PortFolioImage>
+          <img style={{width: '100%'}} src={portFolio.img} alt=""/>
+        </PortFolioImage>
+      ) : ''}
+      <PortFolioInfo id={portFolio.id} >
+        <dl style={{textAlign: 'center'}}>ポートフォリオ詳細</dl>
+        <dl>名前: {portFolio.name}</dl>
+        <dl>内容: {portFolio.kind}</dl>
+        <dl>作成時期: {portFolio.date}</dl>
+        <dl>URL: {portFolio.url}</dl>
+      </PortFolioInfo>
+    </React.Fragment>
   ): (
     <React.Fragment></React.Fragment>
   )
--- src/screen/PortFolioScreen/index.tsx
+++ src/screen/PortFolioScreen/index.tsx
@@ -1,46 +1,55 @@
 import React, { useState }  from 'react'
 import { SelectPortfolio } from './SelectPortfolio/index'
 import { PortFolioDeatils } from './PortFolioDetails/index'
-
-export interface PortFolio {
+ 
+export interface PortFolio { 
   id: number
   name: string
   kind: string
   date: string
   url: string
+  img: string
+  backgroundcolor: string
 }
 
 export const PortFolioScreen = () => {
-  const [ portFolio, setPortFolio ] = useState<PortFolio>({ id: 0, name: '', kind: '', date: '', url: '' })
+  const [ portFolio, setPortFolio ] = useState<PortFolio>({ id: 0, name: '', kind: '', date: '', url: '', img: '', backgroundcolor: '' })
   const portFolios = [
     {
       id: 1,
       name: 'とりま始める大学生通信',
       kind: '台湾留学記',
       date: '2018.9 - 2019.6',
-      url: 'https://www.torimastart.com/'
+      url: 'https://www.torimastart.com/',
+      img: '../../../../public/images/torimastart.png',
+      backgroundcolor: 'lightpink'
     },
     {
       id: 2,
       name: '個人ブログ',
       kind: '現在はプログラミング学習情報が中心。自作したWordPressテーマを使用しています。',
       date: '2019.5 - present',
-
-      url: 'https://ryotaro-blog.xyz/'
+      url: 'https://ryotaro-blog.xyz/',
+      img: '../../../../public/images/ryotaro-blog.png',
+      backgroundcolor: 'lightblue'
     },
     {
       id: 3,
       name: 'Railsポートフォリオ',
       kind: 'プログラミング学習やその他',
       date: '2019.5',
-      url: 'https://tasumane.herokuapp.com/'
+      url: 'https://tasumane.herokuapp.com/',
+      img: '../../../../public/images/tasumane.png',
+      backgroundcolor: 'lightgreen'
     },
     {
       id: 4,
       name: 'Reactポートフォリオ「Ryotaro\'s Home Page」',
       kind: '自己紹介用ポートフォリオ',
       date: '2019.8',
-      url: 'https://ryotaro-blog.xyz/'
+      url: '',
+      img: '',
+      backgroundcolor: 'lightgrey',
     }
   ]
 
