c34b27566f41801745b289eecdbc98e49c36c908
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-08-30 16:07:48 +0900

    fix homescreen

--- src/screen/HomeScreen/index.tsx
+++ src/screen/HomeScreen/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Typed from 'react-typed'
-import { Title } from './styles'
+import { Title, Info, InfoButton } from './styles'
 import ryotaro from '../../images/ryotaro_kitamura.jpg'
 
 export const HomeScreen = () => {
   return (
     <React.Fragment>
-      <div style={{borderBottom: '2px solid white', lineHeight: '100px', fontFamily: 'arial black', padding: '100px'}}>
-        <img style={{ borderRadius: '50%', border: '10px solid black', width: '20%', height: '30%'}} src={ryotaro} alt=""/>
-        <div style={{textAlign: 'end'}}>
-          <div>
+      <div style={{background: 'linear-gradient(-45deg, rgb(255, 49, 2), rgb(255, 234, 84))', borderBottom: '2px solid white', lineHeight: '100px', fontFamily: 'arial black', padding: '30px'}}>
+        <div>
+          <div style={{textAlign: 'center'}}>
+            <img style={{ borderRadius: '50%', border: '10px solid black', width: '20%', height: '30%'}} src={ryotaro} alt=""/>
+          </div>
+          <div style={{textAlign: 'end'}}>
             <Typed
             style={{color: 'black', fontSize: '50px', display: 'inline-block', verticalAlign: 'middle'}}
             strings={['Hello World!']}
@@ -27,13 +29,16 @@ export const HomeScreen = () => {
             </Typed>
           </div>
         </div>
-        <div style={{ borderRadius: '50%', border: '10px solid black'}}></div>
       </div>
-      <div style={{borderBottom: '2px solid white', textAlign: 'center', fontFamily: 'arial black', padding: '200px'}}>
+      <div style={{borderBottom: '2px solid white', textAlign: 'center', fontFamily: 'arial black', padding: '100px'}}>
         <Title>ABOUT ME</Title>
+        <Info>学歴・職歴・プログラミング歴</Info>
+        <InfoButton style={{color: 'black', border: '2px solid black'}}>About Me</InfoButton>
       </div>
-      <div style={{textAlign: 'center', fontFamily: 'arial black', padding: '200px'}}>
+      <div style={{background: 'linear-gradient(-45deg, rgb(255, 49, 2), rgb(255, 234, 84))', textAlign: 'center', fontFamily: 'arial black', padding: '100px'}}>
         <Title>PORTFOLIOS</Title>
+        <Info>ポートフォリオ</Info>
+        <InfoButton style={{color: 'black', border: '2px solid black'}}>PORTFOLIOS</InfoButton>
       </div>
     </React.Fragment>
     
--- src/screen/HomeScreen/styles.js
+++ src/screen/HomeScreen/styles.js
@@ -1,7 +1,24 @@
 import styled from 'styled-components'
+import Button from '@material-ui/core/Button';
+
 
 export const Title = styled.div`
   font-size: 20px;
   border-bottom: 5px solid;
   display: inline-block;
+`
+export const Info = styled.p`
+  font-size: 15px;
+`
+export const InfoButton = styled(Button)`
+  &&{
+    mergin: 20px;
+    background-color: Transparent;
+    background-repeat:no-repeat;
+    cursor: pointer;
+  }
+  &&:hover{
+    opacity: 0.8
+  }
+
 `
