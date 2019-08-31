e070cc7f68aa744b571cf47a762b1094e1f9804a
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-08-31 10:47:54 +0900

    fix header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom' 
+import { RouteButton } from './styles'
+import { HomeScreen } from '../../screen/HomeScreen'
+import { AboutScreen } from '../../screen/AboutScreen/index'
+import { PortFolioScreen } from '../../screen/PortFolioScreen/index'
+import { MyStudyHistory } from '../../screen/AboutScreen/MyStudyHistory/index'
+import { Programming } from '../../screen/AboutScreen/Programming/index'
+
+
+export const Header = () => {
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <div style={{textAlign: 'end'}}>
+          <RouteButton><Link to='/'>Home</Link></RouteButton>
+          <RouteButton><Link to='/about'>About</Link></RouteButton>
+          <RouteButton><Link to='/portfolios'>PortFolios</Link></RouteButton>
+        </div>
+        <Route exact path='/' component={HomeScreen} />
+        <Route exact path='/about' component={AboutScreen} />
+        <Route path='/portfolios' component={PortFolioScreen} />
+        <Route path='/about/mystudyhistory' component={MyStudyHistory} />
+        <Route path='/about/programming' component={Programming} />
+      </BrowserRouter>
+    </React.Fragment>
+  )
+}
