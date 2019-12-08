88c0d99b10be471e1f8aebc4d9dd7eae6a692600
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-12-08 14:53:25 +0900

    fix for using scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Header } from "./components/Header/Header";
-import "./index.scss";
+import "./stylesheets/index.scss";
 import * as serviceWorker from "./serviceWorker";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
-import { HomeScreen } from "./screen/HomeScreen";
 
 library.add(fab, fas, far);
 
