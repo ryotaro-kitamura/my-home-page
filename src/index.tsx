8a0e3f4d5cbd063c2e011e4ab7324c4e34d53adb
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-12-08 14:18:40 +0900

    fix for sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { Header } from './components/Header/Header'
-import './index.css';
-import * as serviceWorker from './serviceWorker'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { HomeScreen } from './screen/HomeScreen';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Header } from "./components/Header/Header";
+import "./index.scss";
+import * as serviceWorker from "./serviceWorker";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { HomeScreen } from "./screen/HomeScreen";
 
 library.add(fab, fas, far);
 
@@ -15,7 +15,7 @@ ReactDOM.render(
   <React.Fragment>
     <Header />
   </React.Fragment>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
