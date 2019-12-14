21b73ce6ced68c93ee377b757c50c6de7256b364
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-12-14 16:02:39 +0900

    made the bases of aboutscreen

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { HomeScreen } from "../../screen/HomeScreen";
-import { AboutScreen } from "../../screen/AboutScreen";
 import { PortFolioScreen } from "../../screen/PortFolioScreen";
 import NavigationBar from "../atoms/NavigationBar";
+import AboutScreen from "../../screen/AboutScreen";
 
 export const App = () => {
   return (
--- src/screen/AboutScreen/index.tsx
+++ src/screen/AboutScreen/index.tsx
@@ -1,27 +1,13 @@
-import React, { useState } from 'react'
-import { AboutScreenDiv } from './styles'
-import　{ BasicInfo } from './BasicInfo/index'
-import { SelectAbout } from './SelectAbout/index'
-import { AboutElementDetails } from './AboutElementDetails/index'
+import React from "react";
+import ryotaro from "../../images/ryotaro_kitamura.jpg";
+import Icon from "../../components/atoms/Icon";
 
-export interface InterfaceAboutElement {
-  Name: string
-}
-
-export const AboutScreen = () => {
-  const AboutElements = ['学歴', 'プログラミング歴']
-  const [SelectedAboutElement, SelectAboutElement] = useState<InterfaceAboutElement>({ Name: '' })
-
+const AboutScreen = () => {
   return (
-    <AboutScreenDiv>
-      <h2 style={{textAlign: 'center'}}>ABOUT ME</h2>
-      <BasicInfo />
-      <h2 style={{textAlign: 'center'}}>MORE INFO</h2>
-      <div style={{textAlign: 'center'}}>
-        <p>ボタンを選択すると、該当の情報をタイムラインで確認できます</p>
-        <SelectAbout AboutElements={AboutElements} SelectedAboutElement={SelectedAboutElement} SelectAboutElement={SelectAboutElement}/>
-      </div>
-      <AboutElementDetails SelectedAboutElement={SelectedAboutElement}/>
-    </AboutScreenDiv>
-  )
-}
+    <div className='AboutScreen'>
+      <Icon className='AboutScreen__Icon' url={ryotaro} />
+    </div>
+  );
+};
+
+export default AboutScreen;
