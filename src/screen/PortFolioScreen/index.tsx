98025cbcf923e9142f595605b59c4e097a31e031
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-03-03 14:19:04 +0900

    delete old portfolio data

--- src/screen/PortFolioScreen/index.tsx
+++ src/screen/PortFolioScreen/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import torimastart from "../../images/torimastart.png";
-import ryotaroblog from "../../images/ryotaro-blog.png";
-import tasumane from "../../images/tasumane-top.png";
-import react_portfolio from "../../images/react-portfolio.png";
-import PortfolioCard from "../../components/molecules/PortfolioCard";
+import React from 'react';
+import torimastart from '../../images/torimastart.png';
+import ryotaroblog from '../../images/ryotaro-blog.png';
+import tasumane from '../../images/tasumane-top.png';
+import react_portfolio from '../../images/react-portfolio.png';
+import PortfolioCard from '../../components/molecules/PortfolioCard';
 
 export interface PortFolio {
   id: number;
@@ -19,47 +19,38 @@ export const PortFolioScreen = () => {
   const portFolios = [
     {
       id: 1,
-      name: "とりま始める大学生通信",
-      text: "台湾留学記",
-      date: "2018.9 - 2019.6",
-      url: "https://www.torimastart.com/",
-      img: torimastart,
-      backgroundcolor: "lightpink"
+      name: '個人ブログ',
+      text:
+        '現在はプログラミング学習情報が中心。自作したWordPressテーマを使用しています。',
+      date: '2019.5 - present',
+      url: 'https://ryotaro-blog.xyz/',
+      img: ryotaroblog,
+      backgroundcolor: 'lightblue'
     },
     {
       id: 2,
-      name: "個人ブログ",
-      text:
-        "現在はプログラミング学習情報が中心。自作したWordPressテーマを使用しています。",
-      date: "2019.5 - present",
-      url: "https://ryotaro-blog.xyz/",
-      img: ryotaroblog,
-      backgroundcolor: "lightblue"
+      name: 'Railsポートフォリオ',
+      text: 'プログラミング学習やその他',
+      date: '2019.5',
+      url: 'https://tasumane.herokuapp.com/',
+      img: tasumane,
+      backgroundcolor: 'lightgreen'
     },
     {
       id: 3,
-      name: "Railsポートフォリオ",
-      text: "プログラミング学習やその他",
-      date: "2019.5",
-      url: "https://tasumane.herokuapp.com/",
-      img: tasumane,
-      backgroundcolor: "lightgreen"
-    },
-    {
-      id: 4,
-      name: "Reactポートフォリオ",
-      text: "自己紹介用",
-      date: "2019.8",
-      url: "https://ryotaro-kitamura.github.io/portfolios",
+      name: 'Reactポートフォリオ',
+      text: '自己紹介用',
+      date: '2019.8',
+      url: 'https://ryotaro-kitamura.github.io/portfolios',
       img: react_portfolio,
-      backgroundcolor: "lightgrey"
+      backgroundcolor: 'lightgrey'
     }
   ];
 
   return (
-    <div className='PortFolioScreen'>
+    <div className="PortFolioScreen">
       <h2>ポートフォリオ集</h2>
-      <div className='PortFolioScreen__CardArea'>
+      <div className="PortFolioScreen__CardArea">
         {portFolios.map(({ text, img, url, name, date }) => (
           <PortfolioCard
             text={text}
