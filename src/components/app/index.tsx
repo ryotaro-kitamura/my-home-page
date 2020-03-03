c0709a7238902104190ffe9a3685d031e4f9ceb8
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-03-03 13:02:11 +0900

    fix margin of global_contents

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { HomeScreen } from "../../screen/HomeScreen";
-import { PortFolioScreen } from "../../screen/PortFolioScreen";
-import NavigationBar from "../atoms/NavigationBar";
-import AboutScreen from "../../screen/AboutScreen";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HomeScreen } from '../../screen/HomeScreen';
+import { PortFolioScreen } from '../../screen/PortFolioScreen';
+import NavigationBar from '../atoms/NavigationBar';
+import AboutScreen from '../../screen/AboutScreen';
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <div className='Global__Header'>
+      <div className="Global__Header">
         <NavigationBar />
       </div>
       <Switch>
-        <div className='Global__Contents'>
-          <Route exact path='/' component={HomeScreen} />
-          <Route path='/about' component={AboutScreen} />
-          <Route path='/portfolios' component={PortFolioScreen} />
+        <Route exact path="/" component={HomeScreen} />
+        <div className="Global__Contents">
+          <Route path="/about" component={AboutScreen} />
+          <Route path="/portfolios" component={PortFolioScreen} />
         </div>
       </Switch>
     </BrowserRouter>
