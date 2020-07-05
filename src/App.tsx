2f41b2ca49f9e846abb823f5c2da526b451a60bb
Ryotaro <k.ryoutaro.g@gmail.com>
2020-07-05 16:25:47 +0900

    App.tsx の作成と index.tsx の修正

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AboutScreen from './screen/AboutScreen';
+import { PortFolioScreen } from './screen/PortFolioScreen';
+import NavigationBar from './components/atoms/NavigationBar';
+
+// fontawsome の設定
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(fab, fas, far);
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="Global__Header">
+        <NavigationBar />
+      </div>
+      <Switch>
+        <div className="Global__Contents">
+          <Route exact path="/" component={AboutScreen} />
+          <Route path="/portfolios" component={PortFolioScreen} />
+        </div>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App
